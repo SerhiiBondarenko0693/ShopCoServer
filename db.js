927070038765ffe743d9150d11bb19ce5c2e8413
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-10-18 12:59:27 +0300

    copy

--- db.js
+++ db.js
@@ -1,12 +1,9 @@
 const { MongoClient } = require('mongodb');
 const config = require("./config");
 
-
 const client = new MongoClient(config.MONGO_CONNECTION_STRING);
 
-
 async function connect() {
-    console.log("MONGO_CONNECTION_STRING",config.MONGO_CONNECTION_STRING);
     await client.connect();
     console.log('Connected to the database');
 }
--- registrationRoutes.js
+++ registrationRoutes.js
@@ -1,6 +1,7 @@
 const {client} = require("./db")
 const bcrypt = require('bcrypt');
 const {validationResult } = require('express-validator');
+const {sendMailServiceLink} = require("./sendMailServise/sendMailServise");
 
 
 const usersDB = client.db('shopco').collection('users')
@@ -32,8 +33,8 @@ const registrationUser = async (req, res) =>{
             const {insertedId} = await usersDB.insertOne(candidate);
             console.log("insertedId", insertedId);
 
-            // send(email,
-            //     ` https://shopcoserver-git-main-chesterfalmen.vercel.app/api/activate/${insertedId}`)
+            sendMailServiceLink(email,
+                `https://shop-co-server-git-main-serhii-bondarenko1.vercel.app//api/activate/${insertedId}`)
 
             return res.send({
                 status: 200,
