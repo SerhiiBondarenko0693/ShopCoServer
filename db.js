882143b13c2ac184153506f3feebf7d605c21ed1
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-10-18 12:50:51 +0300

    copy

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ const config = require("./config");
 console.log("MONGO_CONNECTION_STRING",config.MONGO_CONNECTION_STRING);
 
 const client = new MongoClient(config.MONGO_CONNECTION_STRING);
+console.log("MONGO_CONNECTION_STRING",config.MONGO_CONNECTION_STRING);
 
 async function connect() {
     await client.connect();
