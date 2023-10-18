55e39e31b3d97f440d3996cfe1369a6b4596dd46
Serhii Bondarenko <sergiy.ol.bondarenko@gmail.com>
2023-10-18 12:07:36 +0300

    copy

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const start = async () => {
 start();
 
 
-// module.exports = app;
+module.exports = app;
 
 
-app.listen(3000, () => {
-    console.log(`Server start 3000`);
-});
+// app.listen(3000, () => {
+//     console.log(`Server start 3000`);
+// });
